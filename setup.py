adcb284facea991e3b0a6187eb2933a2f3e5a42a
extremecoder-rgb <hsuswiowkskow@gmail.com>
2025-07-13 00:59:40 +0530

    project structure is ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Subhranil Mondal",
+    author_email="subhranil@gmail.com",
+    packages=find_packages()
+)
