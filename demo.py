87fc36292af344b9168616ecf66b7421e7c604ed
extremecoder-rgb <hsuswiowkskow@gmail.com>
2025-07-13 13:58:28 +0530

    1. Data Ingestion done successfully

--- demo.py
+++ demo.py
@@ -6,3 +6,9 @@
 # logging.warning("This is a warning message.")
 # logging.error("This is an error message.")
 # logging.critical("This is a critical message.")
+
+
+from src.pipline.training_pipeline import TrainPipeline
+
+pipline = TrainPipeline()
+pipline.run_pipeline()
